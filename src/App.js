f765affee62adc4853aa02729633f90e26572bbb
Malena <malena.d@embluemail.com>
2023-03-14 15:14:00 -0300

    Render a test of a signature template

--- src/App.js
+++ src/App.js
@@ -1,18 +1,13 @@
 import './App.css';
-
-// import SignatureTemplate from './components/SignatureTemplate/SignatureTemplate';
-import ComponenteA from './components/ComponenteA/ComponenteA';
+import SignatureTemplate from './components/SignatureTemplate/SignatureTemplate';
+// import ComponenteA from './components/ComponenteA/ComponenteA';
 
 function App() {
   return (
     <div className="App">
-      
-
-      <ComponenteA name='Malena' surname='Díaz' occupation='Web Developer at emBlue' email='diazlopezmalena@gmail.com' instagram='https://www.instagram.com/malenadiazlopez/' webSite='www.myweb.com' phone='000' imageSrc='https://i.ibb.co/9vMvcWK/profile.png'  />      
+      <SignatureTemplate name='Malena' surname='Díaz' occupation='Web Developer at emBlue' email='diazlopezmalena@gmail.com' instagram='https://www.instagram.com/malenadiazlopez/' webSite='www.myweb.com' phone='000' imageSrc='https://i.ibb.co/9vMvcWK/profile.png'  />      
     </div>
   );
 }
 
 export default App;
-
-{/* <SignatureTemplate name='Malena' surname='Díaz' occupation='Web Developer at emBlue' email='diazlopezmalena@gmail.com' instagram='https://www.instagram.com/malenadiazlopez/' webSite='www.myweb.com' phone='000' imageSrc='https://i.ibb.co/9vMvcWK/profile.png'  /> */}
